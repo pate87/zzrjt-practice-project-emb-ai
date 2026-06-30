dfb8980d28c78e08b3bc8fe9ac2127fa55cbd762
pinky <commit@pinkydev.com>
2026-06-30 16:46:15 -0400

    Task 3 - Format the output of the application to json

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # URL: 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
 # Headers: {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
@@ -15,4 +16,18 @@ def sentiment_analyzer(text_to_analyse):
     # Send a POST request to the API with the text and headers 
     response = requests.post(url, json = myobj, headers=header)
     # Return the response text from the API
-    return response.text
+    # return response.text
+
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    # print(formatted_response)
+
+    # Extracting sentiment label and score from the response
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+
+    # Returning a dictionary containing sentiment analysis results 
+    return {
+        'label': label,
+        'score': score
+    }
